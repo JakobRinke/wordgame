91458efaf1811a40155f4ba2f3432c2d62c3d3f2
JakobRinke <61539310+JakobRinke@users.noreply.github.com>
2022-02-05 15:42:58 +0100

    Initial commit

---
+++ script/api.js
@@ -0,0 +1,82 @@
+const gun = Gun(['https://gun-manhattan.herokuapp.com/gun']);
+
+
+
+
+
+function roomExists(roomID)
+{
+    return gun.get("wordgame/rooms/").get(roomID).get("started").once(logRoom)
+}
+
+function createRoom()
+{
+    roomID = parseInt(Math.random() * 100000)
+    gun.get("wordgame/rooms/").get(roomID).get("started").put(false)
+    gun.get("wordgame/rooms/").get(roomID).get("round").put(0)
+    return roomID;
+}
+
+
+function logRoom(data, key)
+{
+    console.log(data!=null)
+    console.log(data)
+}
+
+
+
+function addWord(roomID,word)
+{
+    gun.get("wordgame/rooms/").get(roomID).get("started").once(function(data, key) {
+        if (data==null || data) {console.log(data); return;}
+        gun.get("wordgame/rooms/").get(roomID).get("words").set(word)
+        console.log("set")
+    })
+}
+
+function start(roomID)
+{
+    gun.get("wordgame/rooms/").get(roomID).get("started").put(true)
+}
+
+
+function nextRound(roomID)
+{
+    gun.get("wordgame/rooms/").get(roomID).get("started").once(function(data, key) {
+        if (data==null || !data) {console.log(data);return; }
+        array = gun.get("wordgame/rooms/").get(roomID).get("round").once(function (round, k) {  
+            round++;
+            gun.get("wordgame/rooms/").get(roomID).get("round").put(round)
+        });
+       
+    })
+}
+
+function setRound(roomID, round)
+{
+    gun.get("wordgame/rooms/").get(roomID).get("round").put(round);
+}
+
+
+function lastRound(roomID)
+{
+    gun.get("wordgame/rooms/").get(roomID).get("started").once(function(data, key) {
+        if (data==null || !data) {return; }
+        array = gun.get("wordgame/rooms/").get(roomID).get("round").once(function (round, k) {
+ 
+            if (round-1 < 0) {return; }
+            round--;
+            gun.get("wordgame/rooms/").get(roomID).get("round").put(round)
+
+        });
+       
+    })
+}
+
+function subscribeRoomEvents(roomID, onStartChange, onWordsChange, onRoundChange)
+{
+    gun.get("wordgame/rooms/").get(roomID).get("started").on(onStartChange);
+    gun.get("wordgame/rooms/").get(roomID).get("words").map().on(onWordsChange);
+    gun.get("wordgame/rooms/").get(roomID).get("round").on(onRoundChange);
+}
---
+++ script/room.js
@@ -0,0 +1,127 @@
+const WordInput = document.getElementById("wordInp");
+const WordContainer = document.getElementById("wordContainer");
+const gameStateWordDiv = document.getElementById("gamestate_word");
+const gameStateGameDiv = document.getElementById("gamestate_game");
+
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const create = urlParams.get("create")=="true"
+
+
+const roomID = parseInt(urlParams.get("roomID"));
+
+
+document.getElementById("RoomID").innerHTML = "Raum ID: " + roomID
+
+var Wordlist = [];
+var Round = 0;
+var started = false;
+
+var wordsMixed;
+
+
+function onWordAdd()
+{
+    var word = String(WordInput.value);
+    if (word.trim()=="")
+    {
+        return;
+    }
+    word = word.toLowerCase()
+    addWord(roomID, word)
+    WordInput.value = ""
+}
+
+
+function onWordsChange(data, key)
+{
+    if (!Wordlist.includes(data))
+    {
+        Wordlist.push(data)
+    }
+
+}
+
+function onRoundChange(data, key)
+{
+    Round = data
+    if(!started)
+    {
+        return;
+    }
+
+    try
+    {
+        if (Round >= wordsMixed.length)
+        {
+            setRound(roomID, wordsMixed.length-1)
+            return;
+        }
+    }
+    catch 
+    {
+        if (Round >= Wordlist.length)
+        {
+            setRound(roomID, Wordlist.length-1)
+            return;
+        }
+    }
+
+
+    if(started==true)
+    {
+        try
+        {
+            WordContainer.innerHTML = wordsMixed[Round]
+        }
+        catch(e)
+        {
+            console.log(e)
+            WordContainer.innerHTML = "Fehler, Bitte Neutstarten"
+        }      
+    }
+}
+
+function onStartChange(data, key)
+{
+    started = data
+    console.log(started)
+    if(started)
+    {
+        wordsMixed = shuffle(Wordlist, roomID);
+
+        gameStateGameDiv.classList.add('gamestate_visible');
+        gameStateGameDiv.classList.remove('gamestate_invisible');
+        
+        gameStateWordDiv.classList.add('gamestate_invisible');
+        gameStateWordDiv.classList.remove('gamestate_visible');
+
+        try
+        {
+            WordContainer.innerHTML = wordsMixed[Round]
+        }
+        catch(e)
+        {
+            console.log(e)
+            WordContainer.innerHTML = "Fehler, Bitte Neutstarten"
+        }      
+    }
+    else 
+    {
+        gameStateGameDiv.classList.add('gamestate_invisible');
+        gameStateGameDiv.classList.remove('gamestate_visible');
+        
+        gameStateWordDiv.classList.add('gamestate_visible');
+        gameStateWordDiv.classList.remove('gamestate_invisible');
+    }
+}
+
+
+
+
+
+
+
+
+subscribeRoomEvents(roomID, onStartChange, onWordsChange, onRoundChange);
